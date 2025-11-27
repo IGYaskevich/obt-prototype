c42cbd70addc0f05422c2e461bd515e43ceb79bb
ignatyaskevich <ignat.yaskevich@aviata.business>
2025-11-27 17:18:38 +0500

    убрал корзину и апруверов

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/obt-prototype/',        // <= ВАЖНО: имя репозитория
+    // base: '/obt-prototype/',        // <= ВАЖНО: имя репозитория
     server: { port: 5173 },
 })
